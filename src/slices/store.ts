f65763e9ed62d6b15cb745cf1f438ce39546cb7a
Duy Khuong <nnkhuongduy@gmail.com>
2021-10-06 22:48:20 +0700

    updating redux store

--- src/slices/store.ts
+++ src/slices/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { _99phantramApi } from './api';
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    [_99phantramApi.reducerPath]: _99phantramApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(_99phantramApi.middleware),
 });
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
